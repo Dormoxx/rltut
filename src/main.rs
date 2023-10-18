df9b5e51a52d97377a47117e426b39358e382dc2
Dormoxx <dormoxx@gmail.com>
2023-10-18 01:34:22 -0400

    2.3 done. generated vscode build json

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,40 @@
 
 mod components;
 use components::*;
-mod systems;
-mod worldstate;
-use worldstate::*;
 mod maps;
 use maps::*;
-use rltk::{RltkBuilder, RGB};
+mod player;
+use player::*;
+mod rect;
+use rltk::{GameState, Rltk, RltkBuilder, RGB};
 use specs::prelude::*;
+
+pub struct State {
+    pub ecs: World,
+}
+impl State {
+    pub fn run_systems(&mut self) {
+        self.ecs.maintain();
+    }
+}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+
+        player_input(self, ctx);
+        self.run_systems();
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, rend) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, rend.fg, rend.bg, rend.glyph);
+        }
+    }
+}
 
 fn main() -> rltk::BError {
     let context = RltkBuilder::simple80x50()
@@ -18,12 +45,18 @@ fn main() -> rltk::BError {
 
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    //gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+
+    let (rooms, map) = new_map_rooms_and_corridors();
+    gs.ecs.insert(map);
+    let (player_x, player_y) = rooms[0].center();
 
     gs.ecs
         .create_entity()
-        .with(Position { x: 40, y: 25 })
+        .with(Position {
+            x: player_x,
+            y: player_y,
+        })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
@@ -31,21 +64,6 @@ fn main() -> rltk::BError {
         })
         .with(Player {})
         .build();
-
-    /*for i in 0..10{
-        gs.ecs
-        .create_entity()
-            .with(Position{x: i*7, y: 20})
-            .with(Renderable{
-                glyph: rltk::to_cp437('☺'),
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(LeftMover{})
-            .build();
-    }*/
-
-    gs.ecs.insert(new_map());
 
     rltk::main_loop(context, gs)
 }
